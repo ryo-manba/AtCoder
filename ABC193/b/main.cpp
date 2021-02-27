367bf26103048fd6db1946031e66a170e78f6eaf
Ryomanba <datemanba3@gmail.com>
2021-02-27 22:40:40 +0900

    ABC193

---
+++ ABC193/b/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e15;
+const int MOD = 1e9 + 7;
+int dy[]={0, 0, 1, -1, 0};
+int dx[]={1, -1, 0, 0, 0};
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<ll> a(n), p(n), x(n);
+    rep(i,n) cin >> a[i] >> p[i] >> x[i];
+    ll ans = 1e9;
+
+    for(int i = 0; i < n; i++)
+    {
+        if (a[i] < x[i])
+        {
+            if (ans > p[i])
+                ans = p[i];  
+        }  
+    }
+    if (ans == 1e9)
+        cout << -1 << endl;
+    else 
+        cout << ans << endl;
+    return 0;
+}
