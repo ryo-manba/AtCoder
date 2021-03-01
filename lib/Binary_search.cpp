e17a3fad27f753ce385eb166363e44b6ed0e6f15
Ryomanba <datemanba3@gmail.com>
2021-03-01 18:11:18 +0900

    Binary_search

---
+++ lib/Binary_search.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> a(100,0);
+
+// index が条件を満たすかどうか
+bool isOK(int index, int key) {
+    if (a[index] >= key) return true;
+    else return false;
+}
+
+// 汎用的な二分探索のテンプレ
+int binary_search(int key) {
+    int ng = -1; //「index = 0」が条件を満たすこともあるので、初期値は -1
+    int ok = (int)a.size(); // 「index = a.size()-1」が条件を満たさないこともあるので、初期値は a.size()
+
+    /* ok と ng のどちらが大きいかわからないことを考慮 */
+    while (abs(ok - ng) > 1) {
+        int mid = (ok + ng) / 2;
+
+        if (isOK(mid, key)) ok = mid;
+        else ng = mid;
+    }
+    return ok;
+}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// index が条件を満たすかどうか
+bool isOK(int index, int key) {
+    if (a[index] >= key) return true;
+    else return false;
+}
+
+// 汎用的な二分探索のテンプレ
+int binary_search(int key) {
+    int left = -1; //「index = 0」が条件を満たすこともあるので、初期値は -1
+    int right = (int)a.size(); // 「index = a.size()-1」が条件を満たさないこともあるので、初期値は a.size()
+
+    /* どんな二分探索でもここの書き方を変えずにできる！ */
+    while (right - left > 1) {
+        int mid = left + (right - left) / 2;
+
+        if (isOK(mid, key)) right = mid;
+        else left = mid;
+    }
+
+    /* left は条件を満たさない最大の値、right は条件を満たす最小の値になっている */
+    return right;
+} 
