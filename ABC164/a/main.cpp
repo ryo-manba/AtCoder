958d5d8cb43763053236988449b9c5f4ea191c52
Ryomanba <datemanba3@gmail.com>
2021-03-02 20:16:47 +0900

    ABC164

---
+++ ABC164/a/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main()
+{
+  int s, w;
+  cin >> s >> w;
+  if (s <= w)
+    cout << "unsafe" << endl;
+  else
+    cout << "safe" << endl;
+  return 0;
+}
---
+++ ABC164/b/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main()
+{
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  while (1)
+  {
+    c -= b;
+    if (c <= 0)
+    {
+       cout << "Yes" << endl;
+       return 0;
+    }
+    a -= d;
+    if (a <= 0)
+    {
+      cout << "No" << endl;
+      return 0;
+    }
+  }
+}
