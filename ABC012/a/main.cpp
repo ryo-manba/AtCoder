f604595c9b4cd1a7b9a958c9f87ffcef27da89d7
Ryomanba <datemanba3@gmail.com>
2021-03-03 15:40:53 +0900

    ABC012

---
+++ ABC012/a/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e15;
+const int MOD = 1e9 + 7;
+int dy[]={0, 0, 1, -1, 0};
+int dx[]={1, -1, 0, 0, 0};
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    swap(a,b);
+    cout << a << " " << b << endl;
+    return 0;
+}
