e5e5cd28d4be90262cbd705616e1f45f4ce075f4
Ryomanba <datemanba3@gmail.com>
2021-03-06 17:44:49 +0900

    dp

--- lib/DP.cpp
+++ lib/DP.cpp
@@ -1,33 +1,57 @@
-#include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-using namespace std;
-using ll = long long;
-using P = pair<int, int>;
-const int INF = (int)1e9;
-const ll INFL = (ll)1e15;
-const int MOD = 1e9 + 7;
-int dy[]={0, 0, 1, -1, 0};
-int dx[]={1, -1, 0, 0, 0};
-template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
 
+/* DP(動的計画法)
+     1. DP 配列全体を「DP の種類に応じた初期値」で初期化
+     2. 初期条件を入力
+     3. ループを回す
+     4. テーブルから解を得て出力
+     
+  *再帰的な全探索のイメージが大切
+  *貰うDP　-> ノードへの遷移方法を考える (dp[i - 1])
+  *配るDP　-> ノードからの遷移方法を考える (dp[i + 1])
+*/
+
+// ナップサック問題
+int main()
+{
+    int n;
+    ll w, weight[110], value[110]; // 品物100個
+    ll dp [110][100100] = {0};
+
+    cin >> n >> w;
+    rep(i,n) cin >> weight[i] >> value[i];
+    rep(i, n) {
+        for (int sum_w = 0; sum_w <= w; sum_w++) {
+
+          // i 番目の品物を選ぶ場合
+          // 部分和を添え字に付け加える
+            if (sum_w - weight[i] >= 0) {
+                chmax(dp[i + 1][sum_w], dp[i][sum_w - weight[i]] + value[i]);
+            }
+
+          // i 番目の品物を選ばない場合
+            chmax(dp[i + 1][sum_w], dp[i][sum_w]);
+        }
+    }
+    // 重さがsum_w 以下の総和の最大値
+    cout << dp[n][w] << endl;
+    return 0;
+}
+
+// Frog
 int main()
 {
   int n;
   cin >> n;
-  if (n <= 9)
-    {
-      cout << "Yes" << endl;
-      return 0;
-    }
-  for(int i = 2; i <= 9; i++){
-    for(int j = 2; j <= 9; j++){
-        if(n == i * j){
-          cout << "Yes" << endl;
-          return 0;
-      }
-    }
+  ll h[100010];
+  ll dp[100010];
+  rep(i,n) cin >> h[i];
+  rep(i, 100010) dp[i] = INF;
+
+  dp[0] = 0;
+  for(int i = 1; i < n; i++)
+  {
+    chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+    if (i > 1) chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
   }
-  cout << "No" << endl;
-  return 0;
+  cout << dp[n - 1] << endl;
 }
