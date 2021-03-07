e3a844e2d74a57de21f3ff63562e134c5731b13d
Ryomanba <datemanba3@gmail.com>
2021-03-07 22:06:38 +0900

    ABC130

---
+++ ABC130/b/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e15;
+const int MOD = 1e9 + 7;
+int dy[]={0, 0, 1, -1, 0};
+int dx[]={1, -1, 0, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    vector<int> l(n);
+    int ans = 0, cnt = 0;
+    rep(i,n) cin >> l[i];
+    rep(i,n) {
+        ans += l[i];
+        if (ans > x) {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+    cout << n + 1 << endl;
+    return 0;
+}
