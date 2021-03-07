2aeb8fdb891fb05b4494d1306b1db96e9cac5cd3
Ryomanba <datemanba3@gmail.com>
2021-03-07 11:14:09 +0900

    ABC072

---
+++ ABC072/a/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e15;
+const int MOD = 1e9 + 7;
+int dy[]={0, 0, 1, -1, 0};
+int dx[]={1, -1, 0, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a - b < 0)
+        cout << 0 << endl;
+    else
+        cout << a - b << endl;
+    return 0;
+}
