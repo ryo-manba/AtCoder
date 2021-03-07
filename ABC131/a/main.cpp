95da762e817eea297ae76d1a60dc8b3970fa1928
Ryomanba <datemanba3@gmail.com>
2021-03-07 16:10:55 +0900

    ABC131

---
+++ ABC131/a/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e15;
+const int MOD = 1e9 + 7;
+int dy[]={0, 0, 1, -1, 0};
+int dx[]={1, -1, 0, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    string s;
+    cin >> s;
+    rep(i,s.size() - 1)
+    {
+        if (s[i] == s[i + 1])
+        {
+            cout << "Bad" << endl;
+            return 0;
+        }
+    }
+    cout << "Good" << endl;
+    return 0;
+}
