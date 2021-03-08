b3313f1372058a638d4e368b08feab357173a0f4
Ryomanba <datemanba3@gmail.com>
2021-03-08 19:26:19 +0900

    bit

--- lib/bit.cpp
+++ lib/bit.cpp
@@ -1,9 +1,50 @@
-#include <bits/stdc++.h>
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-using namespace std;
-using ll = long long;
-using P = pair<int, int>;
 
+/* bit全探索
+    条件が2択の時に使える
+    2のべき乗は 1 << n (2^n)
+    &演算子でそこにflagが立っているのかを確かめる
+*/
+
+
+int main()
+{
+    int n, m, k;
+    cin >> n >> m;
+    vector<int> a(m), b(m);
+    rep(i,m) cin >> a[i] >> b[i];
+    cin >> k;
+    vector<int> c(k), d(k);
+    rep(i,k) cin >> c[i] >> d[i];
+
+    int ans = 0;
+    int k2 = 1<<k; // 2^k
+    rep(s,k2)
+    {
+        vector<int> dish(n + 1);
+        
+        // flagが立っていたら d の条件
+        // 立っていなかったら c の条件
+        rep(i,k) 
+        {
+            if (s>>i & 1) dish[d[i]]++;
+            else dish[c[i]]++;
+        }
+
+        // 条件を満たした個数をnowに入れる
+        int now = 0;
+        rep(i,m)
+        {
+            if (dish[a[i]] == 0) continue;
+            if (dish[b[i]] == 0) continue;
+            now++;
+        }
+        chmax(ans, now);
+    }
+    cout << ans << endl;
+    return 0;
+}
+
+// 別パターン
 int main(){
   int n, w;
   cin >> n >> w;
@@ -21,6 +62,6 @@ int main(){
       return 0;
     }
   }
-  cout << "zannnenn" << endl;
+  cout << "NG" << endl;
   return 0;
 }
