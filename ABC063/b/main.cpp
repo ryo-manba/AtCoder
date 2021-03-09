31f2ad5ffef16671059d415a081a7b8ce740a242
Ryomanba <datemanba3@gmail.com>
2021-03-09 19:48:42 +0900

    ABC063

---
+++ ABC063/b/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e15;
+const int MOD = 1e9 + 7;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    string s;
+    cin >> s;
+    set<char> st;
+    rep(i, s.size()) 
+    {
+        st.insert(s[i]);
+    }
+    if (st.size() == s.size())
+        cout << "yes" << endl;
+    else 
+        cout << "no" << endl;
+    return 0;
+}
