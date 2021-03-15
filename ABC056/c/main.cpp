aee9ff11461eb5910bd7a8ef1fc585c133baa481
Ryomanba <datemanba3@gmail.com>
2021-03-15 20:00:48 +0900

    ABC056

---
+++ ABC056/c/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e15;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-8;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    int x;
+    cin >> x;
+    ll num = 0, i;
+    for (i = 1; num < x; i++)
+    {
+        num += i;
+    }
+    cout << --i << endl;
+    return 0;
+}
