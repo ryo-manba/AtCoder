4912a1a7aa5062262e7345ce1f6f3ca01203a920
Ryomanba <datemanba3@gmail.com>
2021-04-05 16:56:39 +0900

    ABC017

---
+++ ABC/ABC017/b/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr), ios::sync_with_stdio(false), cout << fixed << setprecision(20); }; } fast_ios_;
+const int INF = (int)1e9;
+const ll INFL = (ll)1e18;
+const int MOD = 1e9 + 7;
+const double EPS = 1e-10;
+int dx[]={0, 0, -1, 1};
+int dy[]={1, -1, 0, 0};
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
+
+int main()
+{
+    string s;
+    cin >> s;
+    string check = "oku";
+
+    bool choku = false;
+
+    rep(i,s.size())
+    {
+        choku = false;
+        rep(j,3)
+        {
+            if (s[i] == check[j]) choku = true;
+        }
+        if (i < s.size() - 2 && s[i] == 'c' && s[i + 1] == 'h') 
+        {
+            choku = true;
+            i++;
+        }
+        if (!choku)
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    cout << "YES" << endl;
+    return 0;
+}
