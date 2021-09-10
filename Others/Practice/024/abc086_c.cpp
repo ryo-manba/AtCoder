737e0a6809ba826dec51594f99bb8b532f66299e
Ryomanba <datemanba3@gmail.com>
2021-09-11 00:25:03 +0900

    パリティ

--- Others/Practice/024/abc086_c.cpp
+++ Others/Practice/024/abc086_c.cpp
@@ -13,20 +13,25 @@ int dy[]={1, -1, 0, 0};
 template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return true; } return false; }
 template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return true; } return false; }
 
-ll n, k;
-ll a[1010], b[1010];
+ll n;
+ll t[100010], x[100010], y[100010];
 
 int main()
 {
-	cin >> n >> k;
-	rep(i,n) cin >> a[i];
-	rep(i,n) cin >> b[i];
+	cin >> n;
+	for (int i = 1; i <= n; ++i) cin >> t[i] >> x[i] >> y[i];
 
-	ll diff = 0;
-	rep(i,n) diff += abs(a[i] - b[i]);
-
-	if ((diff % 2 == k % 2 && k >= (diff + 2)) || diff == k)
-		cout << "Yes" << endl;
-	else cout << "No" << endl;
+	ll diff = 0, time = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		diff = abs(x[i] - x[i - 1]) + abs(y[i] - y[i - 1]);
+		time = t[i] - t[i - 1];
+		if (!(diff % 2 == time % 2 && time >= diff))
+		{
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
 	return 0;
 }
